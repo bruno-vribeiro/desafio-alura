675f0863708933ccaac8b40c31b3fffd3d89ad3a
bruno-vribeiro <vilhenab8@gmail.com>
2023-01-23 11:28:36 -0300

    finalizando a função encriptar

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-function criptografar() {
-  var input = document.getElementById("input");
-  var texto = input.value;
 
-  var palavra = texto.replace("a","enter");
+  const input = document.getElementById("input");
+  const mensagem = document.getElementsByClassName("div-secundaria img");
+
+  // A letra "e" é convertida para "enter"
+  // A letra "i" é convertida para "imes"
+  // A letra "a" é convertida para "ai"
+  // A letra "o" é convertida para "ober"
+  // A letra "u" é convertida para "ufat"
+
+  function btnEncriptar(){
+    const texto = encriptar(input.value);
+    alert(texto)
+    input.value = "";
+  }
+
+  function encriptar (stringEncriptada){
+    let matrizCodigo = [["e", "enter"],["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+    if(stringEncriptada.includes(matrizCodigo[i][0])){
+      stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+    }
+    
+    }
+    
+    return stringEncriptada;
+ 
+  }
   
-  alert(palavra);
-}
+ 
+
